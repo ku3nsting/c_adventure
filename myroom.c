65e55133479603e2fe4616c8dd79676182a16538
ku3nsting <ku3nsting@users.noreply.github.com>
2017-05-10 15:25:51 -0700

    Add files via upload

--- myroom.c
+++ myroom.c
@@ -44,8 +44,10 @@ int currentRoomIndex;
 int endRoomIndex;
 
 //array of all possible room names
-char *arrayOfRoomNames[] = {"Asparagus", "Broccoli", "Carrot", "Daikon", "Eggplant", "Fennel", "Grapefruit", "Huckleberry", "Jicama", "Kale"};
+char *nameArray[] = {"Asparagus", "Broccoli", "Carrot", "Daikon", "Eggplant", "Fennel", "Grapefruit", "Huckleberry", "Jicama", "Kale"};
+int randomNameArray[NUMBER_OF_ROOMS];
 int randNumArray[NUMBER_OF_ROOMS];
+int numberOfDoors[NUMBER_OF_ROOMS];
 
 
 
@@ -66,6 +68,10 @@ char* newGameDirectory(int suffix){
 	    printf("Something went wrong with directory creation. Exiting program.");
 		exit(SOMETHING_WENT_WRONG);
 	}
+
+    //navigate into appropriate directory
+	chdir(name);
+
 	return name;
 }
 
@@ -77,14 +83,15 @@ char* newGameDirectory(int suffix){
 //  Creates a file within the directory and writes
 //  some data inside of it.
 //*******************************************************
-void newFileForRoom(char* directory, struct room currentRoom){
+void newFileForRoom(struct room currentRoom){
 
-    //navigate into appropriate directory
-	chdir(directory);
+    printf("Got into the function!");
+
 	//sprintf works like printf, but stores string in first parameter instead of printing:
 	//use it to make name of new file
 	char fileName[8];
 	sprintf(fileName, "room_%d", currentRoom.number);
+
 	//make a new file named room_number
 	FILE *pointerToNewFile;
 	pointerToNewFile = fopen(fileName, "w");
@@ -96,28 +103,37 @@ void newFileForRoom(char* directory, struct room currentRoom){
 	}
 
 	//Put the name of the current room in that room's file
-	fprintf(pointerToNewFile, "ROOM NAME: %s\n", currentRoom.roomName);
+	fprintf(pointerToNewFile, "ROOM NAME: %s \n", currentRoom.roomName);
 
 	//print all doors from that room in the file
 	//Right now, I'm only printing connections if they exist
-	int i;
+	int r;
 	//loop through all possible doors from current room
-	for (i = 0; i < MAX_DOORS; i++){
-        if (currentRoom.doors[i] == 0){
+	for (r = 0; r < MAX_DOORS; r++){
+        if (currentRoom.doors[r] == 0){
             //don't do anything, this door does not open
 		}
 		else{
             //there is a room beyond this door!
-            fprintf(pointerToNewFile, "\nCONNECTION %d: ", (i + 1));
-            fprintf(pointerToNewFile, "\t%s\n", arrayOfRooms[i].roomName);
+            fprintf(pointerToNewFile, "CONNECTION %d: ", (r + 1));
+            fprintf(pointerToNewFile, "\t%s\n", arrayOfRooms[r].roomName);
 		}
 	}
 
 	//print the type of room in the file
-	fprintf(pointerToNewFile, "\nROOM TYPE: %s\n", currentRoom.typeOfRoom);
+	if(currentRoom.typeOfRoom == START_ROOM){
+        fprintf(pointerToNewFile, "\nROOM TYPE: START_ROOM\n");
+	}
+    else if(currentRoom.typeOfRoom == END_ROOM){
+        fprintf(pointerToNewFile, "\nROOM TYPE: END_ROOM\n");
+	}
+    else {
+        fprintf(pointerToNewFile, "\nROOM TYPE: MID_ROOM\n");
+	}
+
 
 	//close the file
-	fclose(pointerToNewFile);
+	//fclose(pointerToNewFile);
 	return;
 }
 
@@ -126,21 +142,28 @@ void newFileForRoom(char* directory, struct room currentRoom){
 //
 //  generates an array of random numbers for room
 //  selection
+//  Takes howMany numbers to generate and range -
+//  (0 .. (range - 1)) will be numbers generated with offset 1
+//  increasing offset adds offset to all results, so
+//  range == 3 offset == 3 would generate numbers 3 through 5
+//  new array overwrites array given as 4th parameter
 //*******************************************************
-void randArray(){
+void randArray(int howMany, int range, int offset, int overwriteArray[NUMBER_OF_ROOMS]){
 //array of random numbers to use for room selection
+    int newArray[howMany];
     int i;
-    for(i = 0; i < NUMBER_OF_ROOMS; i++){
+    for(i = 0; i < howMany; i++){
         int randomNum;
-        randomNum = (rand() % 9) + 1;
+        randomNum = (rand() % range) + offset;
+        //keep offset at 1 for 0-range
         //search array to ensure no duplicates
         int x;
-        for(x = 0; x < i; x++){
+        for(x = 0; x <= i; x++){
             while(randNumArray[x] == randomNum){
                 //if number has been used already, generate new numbers until you get one that doesn't match
-                randomNum = (rand() % 9) + 1;
+                randomNum = (rand() % range) + offset;
             }
-            randNumArray[i] = randomNum;
+            overwriteArray[i] = randomNum;
             }
         }
 
@@ -151,19 +174,99 @@ void randArray(){
     //}
 }
 
+//*******************************************************
+//  R O O M  I N I T I A L I Z E R
+//
+//  initializes room objects and
+//*******************************************************
+void roomInitializer(){
+    //generate an array of random numbers to determine room names
+    randArray(NUMBER_OF_ROOMS, 9, 1, randomNameArray);
+
+            //find largest and smallest integers in randomNameArray
+            int largest = 7;
+            int smallest = 3;
+            int idx;
+            for(idx = 0; idx < NUMBER_OF_ROOMS; idx++){
+                if(randomNameArray[idx] < smallest){
+                    smallest = randomNameArray[idx];
+                }
+                if(randomNameArray[idx] > largest){
+                    largest = randomNameArray[idx];
+                }
+            }
+
+    //generate an array of random numbers to determine room doors
+    randArray(NUMBER_OF_ROOMS, 3, 3, numberOfDoors);
+
+    int i;
+    for(i = 0; i < NUMBER_OF_ROOMS; i++){
+        struct room tempRoom;
+        //get random number from random name array
+        //assign that index's name to room
+        int x = randomNameArray[i];
+        strcpy(tempRoom.roomName, nameArray[x]);
+        //printf("assigning name: %s", nameArray[x]);
+
+        //number the room according to where it appears in arrayOfRooms
+        tempRoom.number = (i + 1);
+
+        //decide how many doors the room has
+        //the room has numberOfDoors[i] doors.
+        int doorNumber = numberOfDoors[i];
+        int p;
+        for(p = 0; p < doorNumber; p++){
+            tempRoom.doors[p] = randomNameArray[i + p];
+        }
+
+        //Make it so that largest name index room is start
+        //and smallest name index room is end
+        if(x == largest){
+            tempRoom.typeOfRoom = START_ROOM;
+        }
+        else if(x == smallest){
+            tempRoom.typeOfRoom = END_ROOM;
+        }
+        else{
+            tempRoom.typeOfRoom = MID_ROOM;
+        }
+        //printf("\ntesting temproom name = %s", tempRoom.roomName);
+        arrayOfRooms[i] = tempRoom;
+        //printf("\ntesting arrayOfRooms name = %s", arrayOfRooms[i].roomName);
+    }
+
+}
+
 
 int main(){
 
     //Get and store the processID, then use it to build a new directory
     int processID = getpid();
-    char* targetDirectory = newGameDirectory(processID);
+
+    roomInitializer();
+    //randArray(NUMBER_OF_ROOMS, 4, 3, numberOfDoors);
+
+    //print the array to test
+    //int j;
+    //for(j = 0; j < NUMBER_OF_ROOMS; j++){
+    //    printf("%d, ", numberOfDoors[j]);
+    //}
+
+    printf("All done initializing rooms!\n");
+
+    newGameDirectory(processID);
     printf("All done building directory!\n");
 
-    newFileForRoom(targetDirectory, arrayOfRooms[1]);
+    //generate files for all rooms:
+    int a;
+    for(a = 0; a < NUMBER_OF_ROOMS; a++){
+        newFileForRoom(arrayOfRooms[a]);
+        //printf("\nRoom %s built!\n", arrayOfRooms[a].roomName);
+    }
     printf("All done making files!\n");
 
-    randArray();
-    printf("\nAll done generating random array!\n");
+    //randArray(NUMBER_OF_ROOMS, 9, 1, randNumArray);
+    //printf("\nAll done generating random array!\n");
 
     return 0;
 }
